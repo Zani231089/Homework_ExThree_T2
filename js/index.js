e4a02e2835574a8dc6edb567ba0039ee8b07e252
Zani231089 <231089@virtualwindow.co.za>
2023-05-07 14:09:37 +0200

    Final

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let timmy = 10;
+let taxmanOne = 15;
+let percentageOne = 100;
+
+let groupOne = timmy * taxmanOne;
+let victoryOne = groupOne / percentageOne;
+
+let answerOne = "Timmy owes the Taxman " + "R" + victoryOne;
+
+console.log(answerOne);
+
+let susan = 25;
+let taxmanTwo = 26;
+let percentageTwo = 100;
+
+let groupTwo = susan * taxmanTwo;
+let victoryTwo = groupTwo / percentageTwo;
+
+let answerTwo = "Susan owes the Taxman " + "R" + victoryTwo;
+
+console.log(answerTwo)
+
+let answerThree = victoryOne + victoryTwo
+let finalAnswer = "The Tax Man will claim " + "R" + answerThree + " from Timmy and Susan"
+
+console.log(finalAnswer)
